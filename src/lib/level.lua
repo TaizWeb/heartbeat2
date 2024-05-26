0864f44f3ed1ab2bb3bada9520535073f9243eca
taiz <walter.kerly@protonmail.com>
2024-05-26 02:35:31 -0400

    feat(level): Flesh out main level classes As well as refactor level_properties->properties

--- src/lib/level.lua
+++ src/lib/level.lua
@@ -16,18 +16,44 @@ function Level:writeEntites() end
 --- Get the flags in the level and write them
 function Level:writeFlags() end
 
+--- Get the tiles in the level and load them
+function Level:loadTiles(tileTable)
+	print("loading tiles...")
+end
+
+--- Get the entities in the level and load them
+function Level:loadEntites(entityTable)
+	print("loading entities...")
+end
+
+--- Get the flags in the level and load them
+function Level:loadFlags() end
+
+---Load the level from JSON format
+---@param path string The path to the file, from the root
+---@return boolean success If the loading succeeded
 function Level:loadLevel(path)
 	-- Load the file's content in
 	local file = io.open(path, "r")
 	if not file then
-		return nil, "Could not open file: " .. path
+		return false
 	end
 	local content = file:read("*all")
 	file:close()
 
 	-- Decode it
-	local loaded_level = json.decode(content)
-	print("loaded: " .. loaded_level.level_properties.name)
+	self.data = json.decode(content)
+	print("loaded: " .. self.data.properties.name)
+
+	for key, value in pairs(self.data) do
+		if key == "tiles" then
+			self:loadTiles(value)
+		elseif key == "entities" then
+			self:loadEntites(value)
+		end
+	end
+
+	return true
 end
 
 return Level
